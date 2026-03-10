442377b6ebc9ac2329ca2c65d653ed2cba90224a
NayanPython <choudharyji527@gmail.com>
2026-03-10 16:43:54 +0530

    extract events from website , and saved events in database (made using SQL), and send email update

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import selectorlib
 import smtplib, ssl
 import os
 import time
-
-"INSERT INTO events VALUES('Tiger', 'tiger city', '2026.12.11')" #.commit() to save in database    # SQL query
-"SELECT * FROM events WHERE date='2026.12.10'"   
+import sqlite3
 
 url = "http://programmer100.pythonanywhere.com/tours/"
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
                   ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+connection = sqlite3.connect("data.db")
 
 def scrape(url):
     """ scrape the page source from url """
@@ -40,22 +40,31 @@ def send_email(message):
     print("email is sent")
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
+    connection.commit()
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 if __name__ == "__main__":
     while True:
         scraped = scrape(url)
         extracted = extract(scraped)
         print(extracted)
-        content = read(extracted)
 
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey, new event is found")
-                time.sleep(2)
+            time.sleep(2)
