9abbcfb3635884811ba09f3e293be46dd2d56ec6
NayanPython <choudharyji527@gmail.com>
2026-03-14 09:11:29 +0530

    OBJECT ORIENTED PROGRAMMING (OOP) used to learn about classes

--- main.py
+++ main.py
@@ -11,60 +11,78 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
                   ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect("data.db")
 
-def scrape(url):
-    """ scrape the page source from url """
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+class Event:                                                                  # functions become methods in classes
+    def scrape(self, url):
+        """ scrape the page source from url """
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+class Email:
+    def send(seld, message):
+        host = "smtp.gmail.com"
+        port = 465
 
-    username = "nayan7857@gmail.com"
-    password = "jguieilgubjxffsg"
+        username = "nayan7857@gmail.com"
+        password = "jguieilgubjxffsg"
 
-    receiver = "nayanxyz0@gmail.com"
-    context = ssl.create_default_context()
+        receiver = "nayanxyz0@gmail.com"
+        context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("email is sent")
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("email is sent")
 
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
-    connection.commit()
+class Database:
+    """ __init__ is a special method and is automatically called every time a new instance (object) of the class is created.
+        You do not call __init__ explicitly. Python handles this automatically when you instantiate a class.
 
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+        self is a reference to the current instance (object) of the class, allowing you to access its
+        attributes and methods from within the class definition.
+        self links the methods and attributes defined within the class to the specific object that is calling the method.
+
+      """
+
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)          # class contain args in __init__ method like data_path = data.db
+
+
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
+        connection.commit()
+
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(url)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(url)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database(database_path="data.db")   # class gets argument
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email(message="Hey, new event is found")
+                database.store(extracted)
+                email = Email()
+                email.send(message="Hey, new event is found")
             time.sleep(2)
